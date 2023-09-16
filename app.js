979439a5f5f1872ade46ebe36101e763c8951e3e
NewMatheusED <58958905+NewMatheusED@users.noreply.github.com>
2023-09-16 16:52:09 -0300

    update app.js
    Mudança de comentário escrito errado

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ window.addEventListener('resize', resizeDiv);
 function resizeDiv() {
   var width = window.innerWidth;
   var height = window.innerHeight;
-  var scaleWidth = width / 1280; // 600 é a largura original da div
-  var scaleHeight = height / 720; // 400 é a altura original da div
+  var scaleWidth = width / 1280; // largura original da div
+  var scaleHeight = height / 720; // altura original da div
   var scale = Math.min(scaleWidth, scaleHeight);
 
   var div = document.querySelector('.level');
