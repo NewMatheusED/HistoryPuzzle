26fb4212fd84601b32bb4780671ae624f0d8d0a6
NewMatheusED <58958905+NewMatheusED@users.noreply.github.com>
2023-09-22 11:12:03 -0300

    .

--- app.js
+++ app.js
@@ -10,3 +10,30 @@ function resizeDiv() {
   var div = document.querySelector('.level');
   div.style.transform = 'scale(' + scale + ')';
 }
+
+let draggable = document.getElementById('draggable');
+let level = document.getElementById('level');
+let mouseX = 0;
+let mouseY = 0;
+let offsetX = 0;
+let offsetY = 0;
+
+draggable.addEventListener('mousedown', (e) => {
+  mouseX = e.pageX - level.offsetLeft;
+  mouseY = e.pageY - level.offsetTop;
+  offsetX = draggable.offsetLeft;
+  offsetY = draggable.offsetTop;
+
+  window.addEventListener('mousemove', moveAt);
+  window.addEventListener('mouseup', stopAt);
+});
+
+function moveAt(e) {
+  draggable.style.left = (e.pageX - mouseX + offsetX) + 'px';
+  draggable.style.top = (e.pageY - mouseY + offsetY) + 'px';
+}
+
+function stopAt() {
+  window.removeEventListener('mousemove', moveAt);
+  window.removeEventListener('mouseup', stopAt);
+}
